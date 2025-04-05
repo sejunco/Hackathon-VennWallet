6eaab092e6533375b1a6dcc51ddbe493d40428d3
sejunco <157188282+sejunco@users.noreply.github.com>
2025-04-06 00:02:00 +0800

    Create HomeScreen.js

---
+++ src/src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Header />
+      <p>Selamat datang di VennWallet!</p>
+    </div>
+  );
+}
